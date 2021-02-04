6ecb943e68a35bc4fc8252f9a39ab39db4480d30
sammir07 <almuzammil05@gmail.com>
2021-02-04 12:20:32 +0700

    pertama

--- lib/Hal2.dart
+++ lib/Hal2.dart
@@ -6,20 +6,41 @@ import 'package:flutter/material.dart';
 class Hal2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold (
-      backgroundColor: Colors.red,
-      appBar: AppBar
-        (title: Text('Family'),
-        centerTitle: true,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "GridView",
+          style: TextStyle(color: Colors.white),
+        ),
       ),
-      body: Center
-        (child: Column(mainAxisAlignment: MainAxisAlignment.center, children:
-      [Text('Keluarga Kita',style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, )
-        ,),
-        SizedBox(height: 30.0,),
-        //Icon(Icons.account_tree_sharp, size: 400, color: Colors.blue,)
-        CircleAvatar(backgroundImage: AssetImage("assets/photoshop.png"),radius: 250,)],
-      ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        children: <Widget>[
+          Container(
+            color: Colors.yellowAccent,
+            child: Center(
+              child: Text("1", style: TextStyle(fontSize: 24.0),),
+            ),
+          ),
+          Container(
+            color: Colors.blueAccent,
+            child: Center(
+              child: Text("2", style: TextStyle(fontSize: 24.0),),
+            ),
+          ),
+          Container(
+            color: Colors.brown,
+            child: Center(
+              child: Text("3", style: TextStyle(fontSize: 24.0),),
+            ),
+          ),
+          Container(
+            color: Colors.orange,
+            child: Center(
+              child: Text("4", style: TextStyle(fontSize: 24.0),),
+            ),
+          ),
+        ],
       ),
     );
   }
